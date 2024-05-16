ad0a9dc9cd39dc8fa53c56358e4f8f9e18478732
Pedro Fernandes <v-pedro.fernandes@Bryj.ai>
2024-05-17 00:32:15 +0100

    Remove example app - In order for .gitignore to work properly

--- Example/Pods/Target Support Files/Build-V3-Plugin-Optimizers-ios/Build-V3-Plugin-Optimizers-ios-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Build_V3_Plugin_Optimizers_ios : NSObject
-@end
-@implementation PodsDummy_Build_V3_Plugin_Optimizers_ios
-@end
--- Example/Pods/Target Support Files/Pods-FASDKBuild-Plugin-Optimizers-ios_Tests/Pods-FASDKBuild-Plugin-Optimizers-ios_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FASDKBuild_Plugin_Optimizers_ios_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_FASDKBuild_Plugin_Optimizers_ios_Tests
-@end
