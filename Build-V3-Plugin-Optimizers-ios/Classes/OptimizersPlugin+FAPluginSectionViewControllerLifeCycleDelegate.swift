5c31128aa50d18a9c70e1c5d555a90a973f5f075
Pedro Fernandes <v-pedro.fernandes@Bryj.ai>
2024-05-17 17:48:28 +0100

    Update copyright notice

--- Build-V3-Plugin-Optimizers-ios/Classes/OptimizersPlugin+FAPluginSectionViewControllerLifeCycleDelegate.swift
+++ Build-V3-Plugin-Optimizers-ios/Classes/OptimizersPlugin+FAPluginSectionViewControllerLifeCycleDelegate.swift
@@ -1,9 +1,8 @@
 //
 //  OptimizersPlugin+FAPluginSectionViewControllerLifeCycleDelegate.swift
-//  Build-V3-Plugin-Optimizers-ios
 //
 //  Created by Jérôme Morissard on 16/05/2022.
-//  Copyright © 2022 Bryj.ai. All rights reserved.
+//  Copyright © 2024 Bryj.ai. All rights reserved.
 //
 
 import FASDKBuild_ios
@@ -12,18 +11,18 @@ import WebKit
 
 extension OptimizersPlugin: FAPluginSectionViewControllerLifeCycleDelegate {
     public func lifeCycleWebViewWillBeInitialized(sectionViewController: FASectionViewController, configuration: WKWebViewConfiguration) {}
-    
+
     public func lifeCycleWebViewInitialized(sectionViewController: FASectionViewController) {
         // init the blockers
         applyBlockers(sectionViewController: sectionViewController)
-        
+
         // Disable auto-init of lazySizes
         if self.lazyLoadingConfiguration.forceLazyLoading {
             let classBundle = Bundle(for: OptimizersPlugin.self)
             if let bundle = Bundle(path: classBundle.bundlePath.appending("/Build-V3-Plugin-Optimizers-ios.bundle")) {
                 sectionViewController.webView()!.addJavascriptFileInjectionIfNeeded(resource: "lazysizes", forMainFrameOnly: true, bundle: bundle)
                 sectionViewController.webView()!.addJavascriptFileInjectionIfNeeded(resource: "interface_auto_lazzyload", forMainFrameOnly: true, bundle: bundle)
-                
+
                 var startScript = "FABuild.internal.initLazyLoading();"
                 if lazyLoadingConfiguration.classListToExclude.isEmpty == false {
                     var injectExcludeClasses = "";
@@ -32,7 +31,7 @@ extension OptimizersPlugin: FAPluginSectionViewControllerLifeCycleDelegate {
                     }
                     startScript = "\(injectExcludeClasses); \(startScript)"
                 }
-                
+
                 sectionViewController.webView()!.addJavascriptSourceInjectionIfNeeded(source: startScript, identifier: "FABuild.internal.initLazyLoading", forMainFrameOnly: true, injectionTime: .atDocumentStart)
             }
         }
--- Build-V3-Plugin-Optimizers-ios/Classes/OptimizersPlugin.swift
+++ Build-V3-Plugin-Optimizers-ios/Classes/OptimizersPlugin.swift
@@ -1,9 +1,8 @@
 //
 //  OptimizersPlugin.swift
-//  Build-V3-Plugin-Optimizers-ios
 //
 //  Created by Jérôme Morissard on 16/05/2022.
-//  Copyright © 2022 Bryj.ai. All rights reserved.
+//  Copyright © 2024 Bryj.ai. All rights reserved.
 //
 
 import FASDKBuild_ios
@@ -13,7 +12,7 @@ import WebKit
 public class LazyLoadingConfiguration {
     // This will ignore the images if mentioned classes are exist into classList
     public var classListToExclude = [String]()
-    
+
     //
     public var forceLazyLoading = false
 }
@@ -21,14 +20,13 @@ public class LazyLoadingConfiguration {
 public class OptimizersPlugin: FABasePlugin {
     // Resource Blockers
     public var blockers = [ResourceBlocker]()
-    
+
     // LazyLoading Configuration
     public var lazyLoadingConfiguration = LazyLoadingConfiguration()
-    
+
     public override init() {
         super.init()
         self.sectionLifeCycleDelegate = self
         self.sectionWebViewNavigationDelegate = self
     }
 }
-
